1696e357ff46f322e52ce25bb92dc226cbf80713
Ana Luiza Fernandes <analuizateixeirafernandes184@gmail.com>
2023-03-30 15:05:16 -0300

    feat: create language sticker

--- stickers-generator/src/App.java
+++ stickers-generator/src/App.java
@@ -4,13 +4,14 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String ImdbUrl = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-        String NasaUrl = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-03-09&end_date=2022-03-11";
+        String imdbUrl = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        String nasaUrl = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-03-09&end_date=2022-03-11";
+        String languagesAPIUrl = "http://localhost:8080/api/languages";
 
-        ContentExtractor contentExtractor = new NasaContentExtractor();
+        ContentExtractor contentExtractor = new LanguagesContentExtractor();
         HttpRequestClient httpClient = new HttpRequestClient();
 
-        String json = httpClient.search(NasaUrl);
+        String json = httpClient.search(languagesAPIUrl);
         List<Content> jsonContent = contentExtractor.extract(json);
 
         StickerGenerator stickerGenerator = new StickerGenerator();
